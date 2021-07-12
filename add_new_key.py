680791621ddd3d3d34a54faef406d792d5477df9
MichiMar <michimar.alexis@gmail.com>
2021-07-12 00:49:23 -0500

    agregar nuevo objeto dentro del diccionario

---
+++ add_new_key.py
@@ -0,0 +1,17 @@
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+}
+
+
+teams['red sox'] = ['Price', 'Betts']
+
+teams['otro primero'] = ['uno', 'dos']
+
+print(teams)
+# terminal {  'red sox': ['Price', 'Betts'], 
+#             'otro primero': ['uno', 'dos'], 
+#             'astros': ['Altuve', 'Correa', 'Bregman'], 
+#             'angels': ['Trout', 'Pujols'], 
+#             'yankees': ['Judge', 'Stanton']   }
