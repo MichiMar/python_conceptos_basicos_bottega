526fbdcb9e79a099699bf0cd586547ea1b6e3458
MichiMar <michimar.alexis@gmail.com>
2021-09-07 18:11:42 -0500

    empeando con loops

---
+++ loop.py
@@ -0,0 +1,16 @@
+familia = ['Romi', 'Flor', 'Mar', 'Betty', 'Gian']
+
+for i in familia:
+    print(i)
+
+liderazgo = {
+    '1': 'Flor',
+    '2': 'Romi',
+    '3': 'Betty',
+    '4': 'Gian',
+    '5': 'Mar'
+}
+
+for position, x in liderazgo.items():
+    print('Posicion', position)
+    print('mienbro', x)
